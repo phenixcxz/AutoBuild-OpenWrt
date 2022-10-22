e80bb0ab97cb95304d2ab50cbdb5bdada29297db
phenixcxz <35886291+phenixcxz@users.noreply.github.com>
2022-10-22 14:51:51 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -13,3 +13,6 @@ sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-se
 
 #3. Replace with JerryKuKu’s Argon
 #rm openwrt/package/lean/luci-theme-argon -rf
+
+#修改主机名
+sed -i '/luci commit system/i\uci set system.@system[0].hostname='LEDE'' openwrt/package/lean/default-settings/files/zzz-default-settings
